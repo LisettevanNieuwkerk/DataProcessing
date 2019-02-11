6913ac7ea17305d13c5de0ee137651fdb1990ae4
LisettevanNieuwkerk <lisettevannieuwkerk@gmail.com>
2019-02-11 13:45:59 +0100

    add vis

---
+++ Homework/Week_1/visualizer.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# Name:
+# Student number:
+"""
+This script visualizes data obtained from a .csv file
+"""
+
+
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import csv
+import matplotlib.pyplot as plt
+from statistics import mean
+import numpy as np
+
+# Global constants for the input file, first and last year
+INPUT_CSV = "movies.csv"
+START_YEAR = 2008
+END_YEAR = 2018
+
+# Global dictionary for the data
+data_dict = {str(key): [] for key in range(START_YEAR, END_YEAR)}
+
+if __name__ == "__main__":
+    with open(INPUT_CSV, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data_dict[row['Year']].append(float(row['Rating']))
+    for key in range(START_YEAR, END_YEAR):
+        list = data_dict[str(key)]
+        av = round(mean(list), 1)
+        data_dict[str(key)] = av
+    print(data_dict)
+
+years = [*data_dict]
+rating = [data_dict[x] for x in years]
+
+plt.xlabel('Year')
+plt.ylabel('Rating')
+plt.title('Rating top 50 movies')
+plt.bar(data_dict.keys(), data_dict.values(), align='center', alpha=1)
+plt.yticks(np.arange(min(data_dict.values()), max(data_dict.values()), 0.2))
+plt.show()
