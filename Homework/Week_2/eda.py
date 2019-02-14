c98939f5b98691b2c1c6b686e88ba8c6f8827230
LisettevanNieuwkerk <lisettevannieuwkerk@gmail.com>
2019-02-14 16:00:01 +0100

    csv

--- Homework/Week_2/eda.py
+++ Homework/Week_2/eda.py
@@ -4,17 +4,37 @@
 This script
 """
 
+import pandas as pd
 import csv
 from requests import get
 from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
 
-INPUT_CSV = "input.csv"
+INPUT_FILE = 'input.csv'
+OUTPUT_CSV = 'output.csv'
+
 
 
 if __name__ == "__main__":
-    with open(INPUT_CSV, newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            print(row)
+    with open(INPUT_FILE, 'r', newline='') as input:
+        for lines in input:
+            if lines != '\r\n':
+                with open(OUTPUT_CSV, 'w', newline='') as outfile:
+                    writer = csv.writer(outfile)
+                    writer.writerows([lines])
+
+
+
+    reader = pd.read_csv(INPUT_FILE)
+    print(reader)
+
+
+
+        #data_dict = {str(key): [] for key in range(START_YEAR, END_YEAR)}
+
+
+            #['Country', 'Region', 'Population', 'Area (sq. mi.)', 'Pop. Density (per sq. mi.)',
+            #'Coastline (coast/area ratio)', 'Net migration', 'Infant mortality (per 1000 births)',
+            #'GDP ($ per capita) dollars', 'Literacy (%)', 'Phones (per 1000)', 'Arable (%)', 'Crops (%)', 'Other (%)', 'Climate',
+            #'Birthrate', 'Deathrate', 'Agriculture', 'Industry', 'Service'])
