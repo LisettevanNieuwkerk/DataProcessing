d2daafecb4c639fddc5b3e4ffbd8463401674a7e
LisettevanNieuwkerk <lisettevannieuwkerk@gmail.com>
2019-02-14 21:04:30 +0100

    complete csv

--- Homework/Week_2/eda.py
+++ Homework/Week_2/eda.py
@@ -18,37 +18,12 @@ OUTPUT_CSV = 'output.csv'
 
 
 if __name__ == "__main__":
-    # Write csv correct
-    #file = []
-    #with open(INPUT_FILE, 'r', newline='') as input:
-    #    for line in input:
-    #        print(line)
-
-
-            #if field != ['-']:
-            #    if field[2].isdigit():
-            #        field.remove(field[2])
-            #    file.append(field
-
-    #with open(OUTPUT_CSV, 'w', newline='') as outfile:
-    #    writer = csv.writer(outfile)
-    #    writer.writerow(file[0])
-    #    writer.writerows(file[1:])
-
     df = pd.read_csv(INPUT_FILE)
+    items = ['Country', 'Region', 'Pop. Density (per sq. mi.)',
+    'Infant mortality (per 1000 births)', 'GDP ($ per capita) dollars']
+    df = df.filter(items=items)
+    df = df.dropna()
+    print(df)
 
     with open(OUTPUT_CSV, 'w', newline='') as outfile:
         df.to_csv(OUTPUT_CSV, index=False)
-
-
-    #    writer.writerows(file[1:])
-
-
-
-
-
-
-            #['Country', 'Region', 'Population', 'Area (sq. mi.)', 'Pop. Density (per sq. mi.)',
-            #'Coastline (coast/area ratio)', 'Net migration', 'Infant mortality (per 1000 births)',
-            #'GDP ($ per capita) dollars', 'Literacy (%)', 'Phones (per 1000)', 'Arable (%)', 'Crops (%)', 'Other (%)', 'Climate',
-            #'Birthrate', 'Deathrate', 'Agriculture', 'Industry', 'Service'])
