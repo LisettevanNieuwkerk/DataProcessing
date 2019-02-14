d3a92a39224b78c1f3157b8c7fbeb72b6347c7a8
LisettevanNieuwkerk <lisettevannieuwkerk@gmail.com>
2019-02-14 20:07:37 +0100

    csv

--- Homework/Week_2/eda.py
+++ Homework/Week_2/eda.py
@@ -12,30 +12,37 @@ from contextlib import closing
 from bs4 import BeautifulSoup
 import re
 
-INPUT_FILE = 'input.csv'
+INPUT_FILE = 'input_(1).csv'
 OUTPUT_CSV = 'output.csv'
 
 
 
 if __name__ == "__main__":
     # Write csv correct
-    file = []
-    with open(INPUT_FILE, 'r', newline='') as input:
-        for line in input:
-            fields = line.replace('"', '')
-            fields = fields.replace('  ', '')
-            fields = fields.replace('\r\n', '')
-            field = fields.split(',')
-            if field != ['']:
-                file.append(field)
+    #file = []
+    #with open(INPUT_FILE, 'r', newline='') as input:
+    #    for line in input:
+    #        print(line)
+
+
+            #if field != ['-']:
+            #    if field[2].isdigit():
+            #        field.remove(field[2])
+            #    file.append(field
+
+    #with open(OUTPUT_CSV, 'w', newline='') as outfile:
+    #    writer = csv.writer(outfile)
+    #    writer.writerow(file[0])
+    #    writer.writerows(file[1:])
+
+    df = pd.read_csv(INPUT_FILE)
+    print(df)
 
     with open(OUTPUT_CSV, 'w', newline='') as outfile:
         writer = csv.writer(outfile)
-        writer.writerow(file[0])
-        writer.writerows(file[1:])
-
-    #reader = pd.read_csv(OUTPUT_CSV)
-    #print(reader)
+        writer.writerow(df)
+    print(OUTPUT_CSV)    
+    #    writer.writerows(file[1:])
 
 
 
